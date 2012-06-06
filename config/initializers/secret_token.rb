e5b28849d40f4ca1faa197eed673f2d71b52b3cb
Matt Gillooly <matt@mattgillooly.com>
2012-06-05 23:21:46 -0400

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampfireAlerts::Application.config.secret_token = '2d6846655bbb26c38b3b11dc1bdeda8109850fe719b86d63db5c775b8fb4f715b509821d328f4826a29e909149e594d64501e798f855b20ceab3033fb39ac5f5'
