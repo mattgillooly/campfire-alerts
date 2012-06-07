5ed9e15eb2b1496316d2965dadae8f61cdca0adc
Matt Gillooly <matt@mattgillooly.com>
2012-06-07 10:31:03 -0400

    remove csrf protection from webhook controller

--- app/controllers/postmark_callbacks_controller.rb
+++ app/controllers/postmark_callbacks_controller.rb
@@ -1,6 +1,9 @@
 class PostmarkCallbacksController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   def create
+    logger.info "received web hook from Postmark"
     AlertProcessor.new.call(params['HtmlBody'])
     head :ok
   end
