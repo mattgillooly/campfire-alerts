4d3fd618aa0afc9d82fc00124d61f9e9af095c0b
Matt Gillooly <matt@mattgillooly.com>
2012-06-07 10:29:03 -0400

    logging

--- app/services/alert_processor.rb
+++ app/services/alert_processor.rb
@@ -4,12 +4,15 @@ require 'chatter'
 class AlertProcessor
 
   def call(alert_html)
+    Rails.logger.info "starting AlertProcessor"
     alert = GoogleAlert.new(alert_html)
     chatter = Chatter.new
 
     alert.results.each do |result|
+      Rails.logger.info "Posting link: #{result}"
       chatter.post_link(result.to_s)
     end
+    Rails.logger.info "finished posting links"
   end
 
 end
