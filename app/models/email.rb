c5d16e434637c26c1544302dab6c28620345c059
Matt Gillooly <matt@mattgillooly.com>
2012-06-09 01:21:56 -0400

    add validations around database constraints

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,9 @@
 class Email < ActiveRecord::Base
   attr_accessible :body, :from, :subject
 
+  validates :from, length: { maximum: 255 }
+  validates :subject, length: { maximum: 255 }
+
   validates_presence_of :body, :from, :subject
 
   has_many :links
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,10 @@
 class Link < ActiveRecord::Base
   attr_accessible :blurb, :source, :title, :url, :email
 
+  validates :source, length: { maximum: 255 }
+  validates :title, length: { maximum: 255 }
+  validates :url, length: { maximum: 255 }
+
   validates_presence_of :title, :url
 
   belongs_to :email
